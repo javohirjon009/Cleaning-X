8ffcf1575c37afcfcead8bd849da7da9a9841dcd
javohirjon009 <javohirakramov231@gmail.com>
2025-04-12 02:26:10 +0500

    salom

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text"> Hello react</h1>
+  
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.modules.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="header">Header</h1>
+    </div>
+  );
+}
+
+export default Header;
